8ebe1ffcb7476202b8833ac5f52a0eae75d8c51e
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-02-13 15:12:02 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-node utils/updateData.js movie update_ids update_db
-node utils/updateData.js tvshow update_ids update_db
+SOURCE=$1
+if [[ -z $SOURCE ]]; then
+  node utils/updateData.js movie update_ids update_db
+  node utils/updateData.js tvshow update_ids update_db
+else
+  node utils/updateData.js movie update_ids update_db local
+  node utils/updateData.js tvshow update_ids update_db local
+fi
