5af4d8315b88f5770a5e73e3b00d06be1a9e8b2d
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-04-24 13:51:17 +0200

    feat(run.sh): add auto commit for perf updates only

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 SOURCE=$1
+PUSH=$2
 if [[ -z $SOURCE ]]; then
   node utils/updateData.js movie update_ids update_db
   node utils/updateData.js tvshow update_ids update_db
 else
   node utils/updateData.js movie update_ids update_db local
   node utils/updateData.js tvshow update_ids update_db local
+
+  onlyIdsFiles=$(npm run prettier && git diff ':(exclude)src/assets/*_ids.txt' | wc -l | awk '{print $1}')
+  if [[ $onlyIdsFiles -eq 0 && $PUSH == "push" ]]; then
+    git add . && git commit -m "perf: update dataset" && git push origin main && git push bitbucket main
+  else
+    echo "Changes are more important than updating the ids files, check the diff first."
+  fi
 fi
