558536c895a39af614ef05951e3b1ddc35a532a0
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-04-24 15:22:27 +0200

    feat(run.sh): small updates

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ if [[ -z $SOURCE ]]; then
   node utils/updateData.js movie update_ids update_db
   node utils/updateData.js tvshow update_ids update_db
 else
-  # node utils/updateData.js movie update_ids update_db local
-  # node utils/updateData.js tvshow update_ids update_db local
+  node utils/updateData.js movie update_ids update_db local
+  node utils/updateData.js tvshow update_ids update_db local
 
-  onlyIdsFiles=$(git diff -- . ':(exclude)src/assets/*_ids.txt' ':(exclude)run.sh' | wc -l | awk '{print $1}')
+  onlyIdsFiles=$(git diff -- . ':(exclude)src/assets/*_ids.txt' | wc -l | awk '{print $1}')
   if [[ $onlyIdsFiles -eq 0 && $PUSH == "push" ]]; then
     git add . && git commit -m "perf: update dataset" && git push origin main && git push bitbucket main
   else
