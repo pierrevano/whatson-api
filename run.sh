9d5b5529feb7a74bc8225c04d45e70b1d1c600a7
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-05-31 21:09:45 +0200

    feat: add update-index on ids assets

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ else
 
   onlyIdsFiles=$(git diff -- . ':(exclude)src/assets/*_ids.txt' | wc -l | awk '{print $1}')
   if [[ $onlyIdsFiles -eq 0 && $PUSH == "push" ]]; then
+    git update-index --no-assume-unchanged src/assets/*_ids.txt
     git add . && git commit -m "perf: update dataset" && git push origin main && git push bitbucket main
+    git update-index --assume-unchanged src/assets/*_ids.txt
   else
     echo "Changes are more important than updating the ids files, check the diff first."
   fi
