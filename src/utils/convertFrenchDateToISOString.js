938d5219cb970614ca4c76cb6667ba32f446c519
Pierre Vanoverschelde <pierre.vano@gmail.com>
2024-06-20 17:34:34 +0200

    refactor: optimize converting french date format

--- src/utils/convertFrenchDateToISOString.js
+++ src/utils/convertFrenchDateToISOString.js
@@ -20,46 +20,48 @@ const monthMap = {
 /**
  * Converts a French date string to an ISO 8601 date string.
  *
- * @param {string} frenchDateStr - The date string in French format (e.g., "12 mars 2023" or "juin 1960").
- * @returns {string} - The ISO 8601 formatted date string (e.g., "2023-03-12T00:00:00.000Z").
+ * @param {string} frenchDateStr - The date string in French format (e.g., "12 mars 2023", "juin 1960", or "1960").
+ * @returns {string|null} - The ISO 8601 formatted date string (e.g., "2023-03-12T00:00:00.000Z") or null if the date string is unknown.
  * @throws Will throw an error if the input date string is invalid or if the month is not recognized.
  */
-const convertFrenchDateToISOString = (frenchDateStr) => {
-  if (frenchDateStr.includes("Date de sortie inconnue")) {
+function convertFrenchDateToISOString(frenchDateStr) {
+  if (/Date de sortie inconnue|Prochainement/.test(frenchDateStr)) {
     return null;
   }
 
-  const regexWithDay = /(\d{1,2})\s+([a-zéû]+)\s+(\d{4})/i;
-  const regexWithoutDay = /([a-zéû]+)\s+(\d{4})/i;
+  const parseDate = (regex) => {
+    const match = frenchDateStr.match(regex);
+    return match ? match.slice(1) : null;
+  };
 
-  let match = frenchDateStr.match(regexWithDay);
-  let day = "01"; // Default day is the first day of the month
+  const dayMonthYear = parseDate(/(\d{1,2})\s+([a-zéû]+)\s+(\d{4})/i);
+  const monthYear = parseDate(/([a-zéû]+)\s+(\d{4})/i);
+  const yearOnly = parseDate(/(\d{4})/i);
 
-  if (!match) {
-    match = frenchDateStr.match(regexWithoutDay); // Try matching without the day part
-  }
+  let day = "01";
+  let month, year;
 
-  if (match) {
-    if (match.length === 4) {
-      day = match[1];
-      month = match[2].toLowerCase();
-      year = match[3];
-    } else if (match.length === 3) {
-      month = match[1].toLowerCase();
-      year = match[2];
-    }
-    const monthNumber = monthMap[month];
-
-    if (monthNumber) {
-      const dateStr = `${year}-${monthNumber}-${day.padStart(2, "0")}`;
-      const date = new Date(dateStr);
-      return date.toISOString();
-    } else {
-      throw new Error("Invalid month in the input date string.");
-    }
+  if (dayMonthYear) {
+    [day, month, year] = dayMonthYear;
+  } else if (monthYear) {
+    [month, year] = monthYear;
+  } else if (yearOnly) {
+    [year] = yearOnly;
+    return `${year}-01-01T00:00:00.000Z`;
   } else {
-    throw new Error("Invalid input date string.");
+    throw new Error("Invalid date format in the input date string.");
   }
-};
+
+  month = month && month.toLowerCase();
+  const monthNumber = monthMap[month];
+
+  if (monthNumber) {
+    const dateStr = `${year}-${monthNumber}-${day.padStart(2, "0")}`;
+    const date = new Date(dateStr);
+    return date.toISOString();
+  } else {
+    throw new Error("Invalid month in the input date string.");
+  }
+}
 
 module.exports = { convertFrenchDateToISOString };
