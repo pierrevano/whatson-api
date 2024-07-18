ce8c2cd46750f7a5ad572421ef613e720924034f
Pierre Vanoverschelde <pierre.vano@gmail.com>
2024-07-18 14:06:01 +0200

    feat: add axios retry logs

--- src/utils/getCheerioContent.js
+++ src/utils/getCheerioContent.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const axiosRetry = require("axios-retry").default;
 const cheerio = require("cheerio");
-
 const { logErrors } = require("./logErrors");
 
 /**
@@ -19,6 +18,43 @@ const getCheerioContent = async (url, options, origin) => {
       retries: 50,
       retryDelay: () => 10000,
       retryCondition: (error) => {
+        const retryInfo = {};
+
+        // User agent and error code
+        retryInfo.userAgent =
+          options && options.headers
+            ? options.headers["User-Agent"]
+            : "No User-Agent specified";
+        retryInfo.errorCode = error.response
+          ? error.response.status
+          : "Network Error";
+
+        // IP Address
+        if (error.response && error.response.headers["x-forwarded-for"]) {
+          retryInfo.ipAddress = error.response.headers["x-forwarded-for"];
+        }
+
+        // Request headers
+        if (options && options.headers) {
+          retryInfo.requestHeaders = options.headers;
+        }
+
+        // Response headers
+        if (error.response && error.response.headers) {
+          retryInfo.responseHeaders = error.response.headers;
+        }
+
+        if (retryInfo.errorCode !== 404) {
+          console.log(
+            `Retrying due to error:\n`,
+            `Error Code: ${retryInfo.errorCode}\n`,
+            `User-Agent: ${retryInfo.userAgent}\n`,
+            `IP Address: ${retryInfo.ipAddress || "N/A"}`,
+            `Request Headers: ${JSON.stringify(retryInfo.requestHeaders, null, 2)}\n`,
+            `Response Headers: ${JSON.stringify(retryInfo.responseHeaders, null, 2)}\n`,
+          );
+        }
+
         // Retry only on network errors or server errors except for 404.
         return (
           !error.response ||
@@ -34,21 +70,17 @@ const getCheerioContent = async (url, options, origin) => {
     }
 
     const $ = cheerio.load(response.data);
-
     const endTime = Date.now();
     const executionTime = endTime - startTime;
-
     console.log(
       `${origin} - ${url}:`,
       response.status,
       "- Execution time:",
       executionTime + "ms",
     );
-
     return $;
   } catch (error) {
     logErrors(error, url, origin);
-
     return {
       error: error,
     };
