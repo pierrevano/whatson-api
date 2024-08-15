d9d93919759e6b7963c5c38d50e252eb6da51271
Pierre Vanoverschelde <pierre.vano@gmail.com>
2024-08-15 14:04:03 +0200

    test: update tests

--- index.test.ts
+++ index.test.ts
@@ -941,7 +941,7 @@ const params = {
     },
   },
 
-  not_existing_platforms_should_not_be_more_than_one: {
+  not_existing_platforms_should_not_be_more_than_three: {
     query: `?is_active=true&item_type=movie,tvshow&platforms=${encodeURIComponent("all,Netflix,Canal+")}&limit=${config.maxLimitRemote}`,
     expectedResult: (items) => {
       expect(
@@ -959,7 +959,7 @@ const params = {
       expect(
         allPlatformNames.filter((name) => !config.platforms.includes(name))
           .length,
-      ).toBeLessThanOrEqual(1);
+      ).toBeLessThanOrEqual(3);
     },
   },
 
